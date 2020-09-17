d11a84c9d8919c981ec771f0bec7e4af888f8918
Madara <madaravin@gmail.com>
2020-09-17 21:43:27 +0300

    new repository

---
+++ src/com/accenture/prebootcamp/survivingman/HelloSurvivingman.java
@@ -0,0 +1,13 @@
+package com.accenture.prebootcamp.survivingman;
+
+public class HelloSurvivingman {
+    public static void main(String[] args) {
+
+
+        System.out.println(" HH     HH       A       NN     NN     GG G GG    M         M       A       NN    NN ");
+        System.out.println(" HH H H HH     AA AA     NN NN  NN   GG           MM M   M MM     AA AA     NN N  NN ");
+        System.out.println(" HH H H HH    AA A AA    NN  NN NN   GG    G GG   MM  M M  MM    AA A AA    NN  N NN ");
+        System.out.println(" HH     HH   AA     AA   NN     NN     GG G GG    MM   M   MM   AA     AA   NN    NN ");
+    }
+}
+
