1bee492afe82d3738de08e4a8b54da649316effc
Jaerio <tarasovs.vlad@gmail.com>
2020-09-22 18:30:46 +0300

    Survivingman game

---
+++ src/com/accenture/prebootcamp/survivingman/Survivingman.java
@@ -0,0 +1,136 @@
+
+package com.accenture.prebootcamp.survivingman;
+
+import java.util.Scanner;
+
+public class Survivingman {
+    private static String[] words = {"fire", "silver", "java", "thanks", "dream", "knowledge", "magic",
+            "pizza", "chicken", "nightmare", "computer", "facebook", "strength", "fear", "safety", "holiday",
+            "human", "tasty", "chocolate", "tiger", "explore", "hide", "jungle", "shelter"};
+    private static String word = words[(int) (Math.random() * words.length)];
+    private static String dashes = new String(new char[word.length()]).replace("\0", "_");
+    private static int countWrong = 0;
+    private static int countRight;
+
+    public static void main(String[] args) {
+
+        System.out.println("");
+        System.out.println("    SSSS    UU      UU   RR RR     VV      VV   II   VV      VV   II   NN     NN     GG GG GG    M         M        A        NN      NN ");
+        System.out.println("  SSS       UU      UU   RR   RR    VV    VV    II    VV    VV    II   NNNN   NN    GG           MMM     MMM      AA AA      NNNN    NN ");
+        System.out.println("    SSS     UU      UU   RR RR       VV  VV     II     VV  VV     II   NN NN  NN   GG     GGG    MM M   M MM     AA   AA     NN  NN  NN ");
+        System.out.println("      SSS    UU    UU    RR  RR       VVVV      II      VVVV      II   NN   NNNN    GG      GG   MM  M M  MM    AA AAA AA    NN    NNNN ");
+        System.out.println("   SSSS        UUUU      RR   RR       VV       II       VV       II   NN     NN     GG GG GG    MM   M   MM   AA       AA   NN      NN ");
+
+        System.out.println();
+
+        System.out.println("The first thing you see when you open your eyes is a bright light. Your head feels hazy and your body- heavy.\n" +
+                "You were on your way to fly to see your family, but the last thing you remember is the pilot yelling that the plane is about to crash.\n" +
+                "Strangely, there is no one else around you, but you know that you have been laying there for a while as your mouth is dry and the plane\n" +
+                "isn't burning anymore, there are just smoke coming from the shattered metal parts.\n" +
+                "Your thoughts are disturbed by the sound of branches breaking under someone's feet.\n" +
+                "You hide behind a plane wing and see a tribe coming in your direction.\n" +
+                "They don't look friendly, with spears in their hands and their faces painted red and black.\n" +
+                "You remember reading in a facebook post, that your crazy aunt posted, about tribes that paint their faces this way\n" +
+                "eat human flesh of lost travellers unless...\n" +
+                "Unless you tell them the magic word... what was the magic word?");
+
+        System.out.println();
+
+        Scanner sc = new Scanner(System.in);
+
+        while (countWrong < 7 && dashes.contains("_")) {
+            System.out.println("Try to remember a letter from the magic word");
+            System.out.println(dashes);
+            String guess = sc.next();
+            action(guess);
+        }
+        sc.close();
+    }
+
+    public static void action(String guess) {
+        String newdashes = "";
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == guess.charAt(0)) {
+                newdashes += guess.charAt(0);
+            } else if (dashes.charAt(i) != '_') {
+                newdashes += word.charAt(i);
+            } else {
+                newdashes += "_";
+            }
+        }
+
+        if (dashes.equals(newdashes)) {
+            countWrong++;
+            wrongAnswer();
+        } else {
+            dashes = newdashes;
+            countRight++;
+            rightAnswer();
+        }
+        if (dashes.equals(word)) {
+            System.out.println("Correct! You win! The word was " + word);
+        }
+    }
+    public static void rightAnswer() {
+        if (countRight == 1) {
+            System.out.println("You guessed right! You just found a water source and now you're hydrated.");
+        }
+        if (countRight == 2) {
+            System.out.println("You guessed right! While hiding from the tribe, you meet a shaman who shared his warm campfire.\n" +
+                    " He gave you a blessing for the way forward.");
+        }
+        if (countRight == 3) {
+            System.out.println("You guessed right! It's getting dark outside, so it is time to build a wigwam to sleep in.\n" +
+                            " With few branches, large leaves and a pilots jacket you made a cozy shelter.\n" +
+                    "In your mind you thought you might be a good interior designer.");
+        }
+        if (countRight == 4) {
+            System.out.println("You guessed right! You were hungry, so you catch a frog. You made some french crispy frog leg delicacies.");
+        }
+        if (countRight == 5) {
+            System.out.println("You guessed right! You had the idea to disguise yourself so that the tribe wouldn't notice you so easily.\n" +
+                    "With some mud you made yourself invisible.");
+        }
+        if (countRight == 6) {
+            System.out.println("You guessed the word! Yay! And right when the tribe had found you while you were taking a nap.\n" +
+                    "You tell them the word and they take you to the nearest village where you meet people\n" +
+                    "who are just about to fly to the same place you were going! You're the happiest person on earth...\n" +
+            "And then you wake up. It was all a dream.");
+        }
+    }
+
+    public static void wrongAnswer() {
+        if (countWrong == 1) {
+            System.out.println("Wrong guess! You ate some poisonous berries by accident and now you're feeling sick.");
+        }
+        if (countWrong == 2) {
+            System.out.println("Wrong guess! While hiding from the tribe, you got very cold so you decided to light a campfire.\n" +
+                    " You saw how to do it on television, so you rubbed the stones together for three hours straight for no reason.");
+        }
+        if (countWrong == 3) {
+            System.out.println("Wrong guess! Where to sleep, you thought. My father always told me I'm not a good builder.\n" +
+            "So you find a cave which made you run for your life, because the bear wasn't waiting for any guests.");
+        }
+        if (countWrong == 4) {
+            System.out.println("Wrong guess! You were hungry, so you catch a colorful frog.\n" +
+                    "You ate it, it tasted terrible and you felt quite strange.It made you hallucinate all day long.");
+        }
+        if (countWrong == 5) {
+            System.out.println("Wrong guess! You had the idea to disguise yourself so that the tribe wouldn't notice you so easily.\n" +
+            "With some mud you made yourself look like you came out from some unsuccessful chocolate spa treatment.\n" +
+                    "Even jungle creatures look at you with condemnation.");
+        }
+        if (countWrong == 6) {
+            System.out.println("Wrong guess! While sneaking in the jungle, suddenly you get attacked by a young tigress.\n" +
+                            "She only injures your arm and then leaves you since you smell bad.");
+        }
+        if (countWrong == 7) {
+            System.out.println("Wrong guess! You are awoken from your nap as the tribe has their spears pointed towards you.\n" +
+                    "You have no escape, your pet is nowhere to be seen and you start to think about the afterlife.\n" +
+                            "They bring you to their village, where they have prepared a large pot for you to boil in and\n" +
+            "they will make a delicious borsch from you. While your screaming for your life,\n" +
+            "you suddenly wake up in your bed, at home. It was all just a nightmare and you decide to never read facebook articles again. ");
+        }
+    }
+}
+
